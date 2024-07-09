234513eba16ea5dac9aae1fca9c32d2e8e374de8
Vatsal565 <vatsalbateriwala562005@gmail.com>
2024-07-09 19:23:42 +0530

    honojs-setup

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,24 +1,34 @@
+import { z } from 'zod';
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { error } from 'console';
+import authors from './authors'
+import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
 app
-  .get('/hello', (c) => {
-    return c.json({
-      message: 'Hello Next.js!',
-    })
-  })
-  .get('/hello/:test', (c) => {
-    const test = c.req.param("test")
+  .get('/hello',
+    clerkMiddleware(),
+    (c) => {
+      const auth = getAuth(c);
+
+      if (!auth?.userId) {
+        return c.json({error : "Unauthoriazed"});
+      }
 
     return c.json({
-      message: "Hello World",
-      test: test
+      message: 'Hello Next.js!',
+      userId : auth.userId,
     })
-  })
+  });
+
+app.route('/authors', authors)
+app.route('/books', books)
 
 export const GET = handle(app)
 export const POST = handle(app)
