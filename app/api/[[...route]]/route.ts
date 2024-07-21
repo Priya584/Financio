f909c08d700dd799c0212d22d5af3425876947bc
diya5772 <pansheriyadiya@gmail.com>
2024-07-21 19:49:19 +0530

    categories

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,28 +1,17 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import accounts from './accounts';
+import categories from './categories';
+import { HTTPException } from 'hono/http-exception';
 
-export const runtime = 'edge'
+export const runtime = 'edge';
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/api');
 
-app
-  .get('/hello',
-    clerkMiddleware(),
-    (c) => {
-      const auth = getAuth(c);
+const routes=app
+    .route('/accounts', accounts)
+.route('/categories', categories);
+export const GET = handle(app);
+export const POST = handle(app);
 
-      if (!auth?.userId) {
-        return c.json({error : "Unauthoriazed"});
-      }
-
-    return c.json({
-      message: 'Hello Next.js!',
-      userId : auth.userId,
-    })
-  });
-
-
-
-export const GET = handle(app)
-export const POST = handle(app)
+export type Apptype = typeof routes;
